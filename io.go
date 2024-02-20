137064e0e1445116a685e88c1318ac7bd283c600
Mark LaPerriere <marklap@gmail.com>
2024-02-19 21:31:34 -0500

    only return lines matching filters

--- io.go
+++ io.go
@@ -1,14 +1,73 @@
 package cproject
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"os"
 )
 
 const (
+	// stdBufSize is the number of bytes read at a time when reading the log file.
 	stdBufSize int64 = 4096
-	newline    byte  = '\n'
+	// newline is a newline
+	newline byte = '\n'
 )
+
+// LineBuffer is a wrapper around a bytes.Buffer. The underlying buffer stores bytes in reverse order
+// of how they're found in the log file.
+type LineBuffer struct {
+	buf *bytes.Buffer
+}
+
+// NewLineBufferFromString creates a new buffer with the initial contents set to the provided string.
+func NewLineBufferFromString(s string) *LineBuffer {
+	return &LineBuffer{
+		buf: bytes.NewBufferString(s),
+	}
+}
+
+// NewLineBuffer creates a new empty buffer.
+func NewLineBuffer() *LineBuffer {
+	return &LineBuffer{
+		buf: bytes.NewBuffer([]byte{}),
+	}
+}
+
+// Returns the length of the buffer.
+func (b *LineBuffer) Len() int {
+	return b.buf.Len()
+}
+
+// Reset resets the buffer.
+func (b *LineBuffer) Reset() {
+	b.buf.Reset()
+}
+
+// WriteByte writes a single byte to the buffer.
+func (b *LineBuffer) WriteByte(c byte) error {
+	return b.buf.WriteByte(c)
+}
+
+// String returns the content of the buffer in the correct order (the order they are arranged in the log file).
+func (b *LineBuffer) String() string {
+	bufLen := b.buf.Len()
+	if bufLen == 0 {
+		return ""
+	}
+	if bufLen == 1 {
+		return string(b.buf.String())
+	}
+
+	buf := make([]byte, b.buf.Len())
+	copy(buf, b.buf.Bytes())
+
+	for i, j := 0, bufLen-1; i < j; i, j = i+1, j-1 {
+		buf[i], buf[j] = buf[j], buf[i]
+	}
+
+	return string(buf)
+}
 
 // startPos determines the best starting position of the seek depending on the size of the file.
 // If the file size is less than or equal to the buffer size, we'll read the whole thing.
@@ -36,7 +95,30 @@ func countTrailingNewlines(buf []byte) int64 {
 	return nlCount
 }
 
-func posNthLineFromEnd(file *os.File, numLines int, filters []Filter) (int64, error) {
+// includeLine determines if the line should be included in the output. It expects
+// a LineBuffer. and returns true and the string
+// if it should be included. If it should not be included it will return false and the string.
+func includeLine(lineBuf *LineBuffer, filters []Filter) (bool, string) {
+	lineLen := lineBuf.Len()
+	if lineLen == 0 {
+		return false, ""
+	}
+
+	line := lineBuf.String()
+	if len(filters) == 0 {
+		return true, line
+	}
+
+	for _, filter := range filters {
+		if filter.Include(line) {
+			return true, line
+		}
+	}
+
+	return false, line
+}
+
+func yieldLines(file *os.File, numLines int, filters []Filter, lines chan<- string, errChan chan<- error) {
 	// ensure we rewind the pointer when we're done
 	defer func() { file.Seek(0, io.SeekStart) }()
 
@@ -51,20 +133,32 @@ func posNthLineFromEnd(file *os.File, numLines int, filters []Filter) (int64, er
 
 	pos, err := startPos(bufSz, file)
 	if err != nil {
-		return -1, err
+		close(lines)
+		errChan <- err
+		close(errChan)
+		return
 	}
 	if pos > 0 {
 		pos, err = file.Seek(-pos, io.SeekEnd)
 		if err != nil {
-			return -1, err
+			close(lines)
+			errChan <- err
+			close(errChan)
+			return
 		}
 	}
+
+	lineBuf := NewLineBuffer()
 
 	for {
 		// after this, the seek pos will be pos + sz
 		sz, err := file.Read(buf)
 		if err != nil {
-			return -1, err
+			if err != io.EOF {
+				close(lines)
+				errChan <- err
+				close(errChan)
+			}
 		}
 
 		start, end := sz-1, 0
@@ -75,18 +169,36 @@ func posNthLineFromEnd(file *os.File, numLines int, filters []Filter) (int64, er
 
 		for i := start; i >= end; i-- {
 			if buf[i] == newline {
-				nlCount++
-				if nlCount == numLines {
-					return pos + int64(i) + 1, nil
+				if include, line := includeLine(lineBuf, filters); include {
+					fmt.Printf("[numLines: %d] include line: %s\n", numLines, line)
+					lines <- line
+					nlCount++
+					if nlCount == numLines {
+						close(lines)
+						close(errChan)
+						return
+					}
+				}
+				lineBuf.Reset()
+			} else {
+				err := lineBuf.WriteByte(buf[i])
+				if err != nil {
+					close(lines)
+					errChan <- err
+					close(errChan)
 				}
 			}
 		}
 
 		// if we've read less than a full buffer size then we've truncated the buffer
-		// on the previous pass and reached the beginning of the file and we haven't counted
-		// the number of lines we're targetting so we should just dump the whole file (i.e. seek to pos 0)
+		// on the previous pass and reached the beginning of the file and we're done.'
 		if sz < int(stdBufSize) {
-			return 0, nil
+			if include, line := includeLine(lineBuf, filters); include {
+				lines <- line
+			}
+			close(lines)
+			close(errChan)
+			return
 		}
 
 		// determine how far to move the pointer by seeing if there's enough bytes to fill the buffer
@@ -100,7 +212,9 @@ func posNthLineFromEnd(file *os.File, numLines int, filters []Filter) (int64, er
 			pos, err = file.Seek(0, io.SeekStart)
 		}
 		if err != nil {
-			return -1, err
+			close(lines)
+			errChan <- err
+			close(errChan)
 		}
 	}
 }
--- io_test.go
+++ io_test.go
@@ -91,53 +91,225 @@ func TestStartPos(t *testing.T) {
 	}
 }
 
-func TestPosNthLineFromEnd(t *testing.T) {
-	content := FxtContent()
-	file, err := FxtFile(t, content)
-	if err != nil {
-		t.Error(err)
-	}
-
-	logFile, err := FxtLogFile(file.Name(), file)
-	if err != nil {
-		t.Error(err)
-	}
-
+func TestIncludeLine(t *testing.T) {
 	testCases := []struct {
-		desc  string
-		lines int
-		want  int64
+		desc     string
+		buf      *LineBuffer
+		filters  []Filter
+		wantBool bool
+		wantStr  string
 	}{
 		{
-			desc:  "lastOne",
-			lines: 1,
-			want:  82,
+			desc:     "noFilters",
+			buf:      NewLineBufferFromString("cba"),
+			filters:  nil,
+			wantBool: true,
+			wantStr:  "abc",
 		}, {
-			desc:  "lastTwo",
-			lines: 2,
-			want:  67,
+			desc:     "emptyBuffer",
+			buf:      NewLineBuffer(),
+			filters:  nil,
+			wantBool: false,
+			wantStr:  "",
 		}, {
-			desc:  "lastThree",
-			lines: 3,
-			want:  47,
+			desc: "singleFilterMatch",
+			buf:  NewLineBufferFromString("cba"),
+			filters: []Filter{
+				NewMatchAnySubstring(WithSubstrings([]string{"abc"})),
+			},
+			wantBool: true,
+			wantStr:  "abc",
 		}, {
-			desc:  "sameAsActual",
-			lines: 4,
-			want:  0,
+			desc: "singleFilterNoMatch",
+			buf:  NewLineBufferFromString("cba"),
+			filters: []Filter{
+				NewMatchAnySubstring(WithSubstrings([]string{"gorilla"})),
+			},
+			wantBool: false,
+			wantStr:  "abc",
 		}, {
-			desc:  "moreThanActual",
-			lines: 10,
-			want:  0,
+			desc: "multipleFilterMatch",
+			buf:  NewLineBufferFromString("cba"),
+			filters: []Filter{
+				NewMatchAnySubstring(WithSubstrings([]string{"gorilla"})),
+				NewMatchAnySubstring(WithSubstrings([]string{"abc"})),
+			},
+			wantBool: true,
+			wantStr:  "abc",
+		}, {
+			desc: "multipleFilterNoMatch",
+			buf:  NewLineBufferFromString("cba"),
+			filters: []Filter{
+				NewMatchAnySubstring(WithSubstrings([]string{"gorilla"})),
+				NewMatchAnySubstring(WithSubstrings([]string{"monkey"})),
+			},
+			wantBool: false,
+			wantStr:  "abc",
 		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
-			got, err := posNthLineFromEnd(logFile.file, tC.lines, nil)
+			gotBool, gotStr := includeLine(tC.buf, tC.filters)
+			if tC.wantBool != gotBool {
+				t.Errorf("unexpected include line boolean - want: %t, got: %t", tC.wantBool, gotBool)
+			}
+			if tC.wantStr != gotStr {
+				t.Errorf("unexpected include line string - want: %s, got: %s", tC.wantStr, gotStr)
+			}
+		})
+	}
+}
+
+func TestYieldLines(t *testing.T) {
+	testCases := []struct {
+		desc  string
+		lines int
+		want  []string
+	}{
+		{
+			desc:  "lastOne",
+			lines: 1,
+			want: []string{
+				"and off-by-1 errors.",
+			},
+		}, {
+			desc:  "lastTwo",
+			lines: 2,
+			want: []string{
+				"and off-by-1 errors.",
+				"naming things,",
+			},
+		}, {
+			desc:  "lastThree",
+			lines: 3,
+			want: []string{
+				"and off-by-1 errors.",
+				"naming things,",
+				"cache invalidation,",
+			},
+		}, {
+			desc:  "sameAsActual",
+			lines: 4,
+			want: []string{
+				"and off-by-1 errors.",
+				"naming things,",
+				"cache invalidation,",
+				"There are 2 hard problems in computer science:",
+			},
+		}, {
+			desc:  "moreThanActual",
+			lines: 10,
+			want: []string{
+				"and off-by-1 errors.",
+				"naming things,",
+				"cache invalidation,",
+				"There are 2 hard problems in computer science:",
+			},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			content := FxtContent()
+			file, err := FxtFile(t, content)
 			if err != nil {
 				t.Error(err)
 			}
-			if tC.want != got {
-				t.Errorf("unexpected position for count of lines - want: %d, got: %d", tC.want, got)
+
+			logFile, err := FxtLogFile(file.Name(), file)
+			if err != nil {
+				t.Error(err)
+			}
+
+			lines := make(chan string, 1)
+			errChan := make(chan error, 1)
+
+			go yieldLines(logFile.file, tC.lines, nil, lines, errChan)
+
+			var got []string
+
+			for line := range lines {
+				got = append(got, line)
+			}
+			err = <-errChan
+			if err != nil {
+				t.Error(err)
+			}
+
+			if !StringSlicesEqual(tC.want, got) {
+				t.Errorf("unexpected results - want: %#v, got: %#v", tC.want, got)
+			}
+		})
+	}
+}
+
+func TestYieldLinesFiltered(t *testing.T) {
+	testCases := []struct {
+		desc    string
+		lines   int
+		filters []Filter
+		want    []string
+	}{
+		{
+			desc:  "lastOneMoreMatches",
+			lines: 1,
+			filters: []Filter{
+				NewMatchAnySubstring(WithSubstrings([]string{"cache", "thing"})),
+			},
+			want: []string{
+				"naming things,",
+			},
+		}, {
+			desc:  "lastTwoSameMatches",
+			lines: 2,
+			filters: []Filter{
+				NewMatchAnySubstring(WithSubstrings([]string{"cache", "thing"})),
+			},
+			want: []string{
+				"naming things,",
+				"cache invalidation,",
+			},
+		}, {
+			desc:  "lastThreeLessMatches",
+			lines: 3,
+			filters: []Filter{
+				NewMatchAnySubstring(WithSubstrings([]string{"cache", "thing"})),
+			},
+			want: []string{
+				"naming things,",
+				"cache invalidation,",
+			},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			content := FxtContent()
+			file, err := FxtFile(t, content)
+			if err != nil {
+				t.Error(err)
+			}
+
+			logFile, err := FxtLogFile(file.Name(), file)
+			if err != nil {
+				t.Error(err)
+			}
+
+			lines := make(chan string, 1)
+			errChan := make(chan error, 1)
+
+			go yieldLines(logFile.file, tC.lines, tC.filters, lines, errChan)
+
+			var got []string
+
+			for line := range lines {
+				got = append(got, line)
+			}
+			err = <-errChan
+			if err != nil {
+				t.Error(err)
+			}
+
+			if !StringSlicesEqual(tC.want, got) {
+				t.Errorf("unexpected results - want: %#v, got: %#v", tC.want, got)
 			}
 		})
 	}
